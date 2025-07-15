ba5450062350563003df9dd596b49c258d0040dc
khaphan-github <phanhoangkha01@gmail.com>
2025-07-15 16:25:56 +0000

    feat: update setup script to install Python 3.11 and create a virtual environment

--- crawler/setup.sh
+++ crawler/setup.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 set -e
-sudo apt update && sudo apt install -y python3 && sudo apt install -y python-pip
+sudo apt update && sudo apt install -y python3.11 python3.11-venv
+python3.11 -m venv .venv
+source .venv/bin/activate
 pip install -r requirements.txt
-supervisord -c supervisord.conf 
+supervisord -c supervisord.conf
