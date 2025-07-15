2deb183de60e7a93a12809d1b87033238f988652
khaphan-github <phanhoangkha01@gmail.com>
2025-07-15 16:21:39 +0000

    feat: enhance setup script to include Python pip installation

--- crawler/setup.sh
+++ crawler/setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
-sudo apt update && sudo apt install -y python3
+sudo apt update && sudo apt install -y python3 && sudo apt install -y python-pip
 pip install -r requirements.txt
 supervisord -c supervisord.conf 
